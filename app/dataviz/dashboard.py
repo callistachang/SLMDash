243a3b9849ae289c9a4005643f312e5622a2d127
callistachang <changcallista@gmail.com>
2020-07-16 00:39:57 +0800

    uploading csv file works! :grin:
    just left with making the graphs respond to the new data.

--- app/dataviz/dashboard.py
+++ app/dataviz/dashboard.py
@@ -4,20 +4,20 @@ import pandas as pd
 import datetime
 
 import dash
+from dash.dependencies import Input, Output, State
 import dash_html_components as html
 import dash_core_components as dcc
 
-from .data import parse_data_sheet
 from .layout import html_layout
 from . import graphs
-from .utils import create_placeholder_df
+from .utils import create_placeholder_df, parse_data_sheet
 
 def create_dashboard(server):
     """Create a Plotly Dash dashboard."""
 
     dash_app = dash.Dash(
         server=server,
-        routes_pathname_prefix='/dashapp/',
+        routes_pathname_prefix='/dataviz/',
         external_stylesheets=['https://codepen.io/chriddyp/pen/bWLwgP.css']
     )
 
@@ -26,6 +26,8 @@ def create_dashboard(server):
 
     # Create Layout
     dash_app.layout = create_layout()
+
+    init_callbacks(dash_app)
 
     return dash_app.server
 
@@ -36,10 +38,10 @@ def create_layout():
 
     return html.Div([
         dcc.Upload(
-            id='upload-data',
+            id='upload-input',
             children=html.Div([
                 'Drag and Drop or ',
-                html.A('Select Files')
+                html.A('Select File')
             ]),
             style={
                 'width': '80%',
@@ -50,12 +52,24 @@ def create_layout():
                 'textAlign': 'center',
                 'margin': '10px',
                 'alignItems': 'center',
-            },
+            }
         ),
         dcc.Graph(
             id='example-graph',
             figure=graphs.pressure_and_oxygen_over_time(df)
-        )
+        ),
+        html.Div(id='upload-output')
     ])
 
-
+def init_callbacks(app):
+    @app.callback(
+        Output('upload-output', 'children'),
+        [Input('upload-input', 'contents'),
+         Input('upload-input', 'filename')]
+    )
+    def update_output(list_of_contents, list_of_names):
+        print("Updating...")
+        if list_of_names is not None:
+            print(list_of_names)
+            df = parse_data_sheet(list_of_contents, list_of_names)
+            print(df.head())
--- app/dataviz/utils.py
+++ app/dataviz/utils.py
@@ -1,4 +1,20 @@
+import pandas as pd
+import numpy as np
+import datetime
+import base64
+import io
+
 import dash_table
+import dash_html_components as html
+
+column_names = ['Time', 'Pressure', 'Filter Status', 'Gas flow speed', 'Gas pump power',
+                'Oxygen top', 'Oxygen 2', 'Gas Temp', 'Platform', 'Build Chamber',
+                'Optical Bench', 'Collimator', 'T_U', 'T_LL', 'T_LR', 'R_LL', 'R_LR',
+                'B_F', 'B_R', 'Pump', 'Cabinet', 'Cabinet 2', 'Ambiance', 'MemTotal',
+                'MemProcess', 'Laser Emission Flags', 'Laser On Flags', 'Galvo X0',
+                'Galvo Y0', 'Servo X0', 'Servo Y0', 'Optic1 Home-in X1',
+                'Optic1 Home-in Y1', 'Optic1 Home-in X2', 'Optic1 Home-in Y2']
+
 
 def create_data_table(df):
     """Create Dash datatable from Pandas DataFrame."""
@@ -13,6 +29,23 @@ def create_data_table(df):
 
     return table
 
+def parse_data_sheet(content, filename):
+    _, content_string = content.split(',')
+    decoded_data = base64.b64decode(content_string)
+
+    try:
+        if 'csv' in filename[-3:]:
+            df = pd.read_csv(io.StringIO(decoded_data.decode('utf-8')), delimiter=";")
+            if all(df.columns == column_names):
+                print('File loaded successfully!')
+                return df
+
+    except Exception as e:
+        print(e)
+
+    print('The file you uploaded was either not a CSV file or does not have the expected column names.')
+    return None
+
+
 def create_placeholder_df():
-    import pandas as pd
     return pd.read_csv("data/sensordata.csv")
