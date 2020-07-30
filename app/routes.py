abf4ccca2cb1d9230dcc7fc618b446340c085924
callistachang <changcallista@gmail.com>
2020-07-30 13:55:42 +0800

    github actions test 1

--- app/routes.py
+++ app/routes.py
@@ -3,12 +3,10 @@ from flask import render_template
 from flask import current_app as app
 
 
-@app.route('/')
+@app.route("/")
 def home():
     """Landing page."""
-    
+
     return render_template(
-        'index.jinja2',
-        title='MVP Dashboard',
-        template='home-template',
+        "index.jinja2", title="MVP Dashboard", template="home-template",
     )
