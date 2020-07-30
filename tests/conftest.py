fe5009924a3fec83893cc6600129dea3325e6663
callistachang <changcallista@gmail.com>
2020-07-30 14:36:48 +0800

    github actions test 5

---
+++ tests/conftest.py
@@ -0,0 +1,12 @@
+import pytest
+
+from app import create_app
+
+
+@pytest.fixture
+def app():
+    yield create_app()
+
+@pytest.fixture
+def client(app):
+    return app.test_client()
---
+++ tests/test_app.py
@@ -0,0 +1,7 @@
+def test_index_page(app, client):
+    res = client.get('/')
+    assert res.status_code == 200
+
+def test_dataviz_page(app, client):
+    res = client.get('/dataviz/')
+    assert res.status_code == 200
