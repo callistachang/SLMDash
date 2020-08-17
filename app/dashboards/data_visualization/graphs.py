c37f4e8ca0bfa199d145c22390cd6588aca98bf3
callistachang <changcallista@gmail.com>
2020-08-17 12:18:27 +0800

    QOL changes

--- app/dashboards/data_visualization/graphs.py
+++ app/dashboards/data_visualization/graphs.py
@@ -1,18 +1,43 @@
-from .constants import TEMP_COLUMNS
+from .constants import TEMP_COLUMNS, COLUMNS_TO_KEEP
+
+# 9 colors for 9 columns
+COLORS = [
+    "#e41a1c",
+    "#377eb8",
+    "#4daf4a",
+    "#984ea3",
+    "#ff7f00",
+    "#ffff33",
+    "#a65628",
+    "#f781bf",
+    "#999999",
+]
+
+COLUMNS = COLUMNS_TO_KEEP[:]
+COLUMNS.remove("Time")
+COLOR_MAPPER = dict(zip(COLUMNS, COLORS))
 
 
 def main_graph(df):
+    print("main_graph()")
     columns = [col for col in df.columns if not col in TEMP_COLUMNS]
+    print(COLOR_MAPPER[columns[0]])
 
     data = [
-        {"x": df["Time"], "y": df[col], "name": col, "type": "scattergl"}
-        for col in columns
+        {
+            "x": df["Time"],
+            "y": df[columns[i]],
+            "name": columns[i],
+            "line": {"color": COLOR_MAPPER[columns[i]]},
+            "type": "scattergl",
+        }
+        for i in range(len(columns))
     ]
 
     layout = {
         "title": f"{', '.join(columns)} Over Time",
         "xaxis": {"title": "Time"},
-        "yaxis": {"title": ", ".join(columns),},
+        "yaxis": {"title": ", ".join(columns)},
     }
 
     return {"data": data, "layout": layout}
