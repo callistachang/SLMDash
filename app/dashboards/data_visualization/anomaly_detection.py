e5c6c2294803aca755b2738e7ac4c5be1e78e3ca
callistachang <changcallista@gmail.com>
2020-08-26 10:09:37 +0800

    change anomaly_detection.py

--- app/dashboards/data_visualization/anomaly_detection.py
+++ app/dashboards/data_visualization/anomaly_detection.py
@@ -16,42 +16,26 @@ def detect_anomalies(input_df, col, t1, t2):
     )
     col_df = input_df.loc[:, col].values.reshape(-1, 1)
     mod.fit(col_df)
-    df = pd.DataFrame()
-    df["Time"] = input_df["Time"]
-    df["score"] = mod.decision_function(col_df)
-    df["actual"] = col_df
-    df["anomaly"] = mod.predict(col_df)
-    df, anomalies = classify_anomalies(df, t1, t2)
-    return df
+    pred = mod.predict(col_df)
+    anomaly = pd.DataFrame()
+    anomaly["Time"] = input_df["Time"]
 
+    # Find decision function to find the score and classify anomalies
+    anomaly["score"] = mod.decision_function(col_df)
+    anomaly["actual"] = col_df
+    anomaly["anomaly"] = pred
 
-def classify_anomalies(df, t1, t2):
-    df = df.sort_values(by="Time", ascending=True)
-    # Categorise anomalies as 0 - no anomaly, 2 - anomaly
-    df["anomaly"].loc[df["actual"].between(t1, t2) == False] = 2
-    df["anomaly"].loc[(df["anomaly"] == 1) & (df["actual"].between(t1, t2) == True)] = 0
-    df["anomaly"].loc[
-        (df["anomaly"] == -1) & (df["actual"].between(t1, t2) == True)
+    # Categorise anomalies as 0-no anomaly, 2-anomaly
+    anomaly["anomaly"].loc[(anomaly["anomaly"] == 1)] = 0
+    anomaly["anomaly"].loc[(anomaly["anomaly"] == -1)] = 2
+    anomaly["anomaly"].loc[anomaly["actual"].between(t1, t2) == False] = 2
+    anomaly["anomaly"].loc[
+        ((anomaly["anomaly"] == 0) | (anomaly["anomaly"] == 2))
+        & (anomaly["actual"].between(t1, t2) == True)
     ] = 0
-    df["anomaly"].loc[
-        (df["anomaly"] == -1) & (df["actual"].between(t1, t2) == False)
+    anomaly["anomaly"].loc[
+        (anomaly["anomaly"] == 2) & (anomaly["actual"].between(t1, t2) == False)
     ] = 2
-    # Shift actuals by one timestamp to find the percentage chage between current and previous data point
-    df["shift"] = df["actual"].shift(-1)
-    df["percentage_change"] = ((df["actual"] - df["shift"]) / df["actual"]) * 100
-    df["percentage_change"].loc[df["percentage_change"].isnull()] = 0
-    dates = df.Time
-    # identify the anomaly points and create a array of its values for plot
-    bool_array = abs(df["anomaly"]) > 0
-    actuals = df["actual"][: len(bool_array)]
-    anomaly_points = bool_array * actuals
-    df["anomaly_points"] = anomaly_points
-    anomalies = pd.DataFrame()
-    anomalies["Time"] = df["Time"][4000:]
-    anomalies["percentage_change"] = df["percentage_change"][4000:]
-    anomalies["anomaly_points"] = df["anomaly_points"][4000:]
-    anomalies = anomalies[anomalies["anomaly_points"] != 0]
-    df["anomaly_points"].loc[df["anomaly_points"] == 0] = np.nan
-    print(anomalies)
-    return df, anomalies
+    anomaly = anomaly[anomaly["anomaly"] != 0]
+    return anomaly["Time"]
 
